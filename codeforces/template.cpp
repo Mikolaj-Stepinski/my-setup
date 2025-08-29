7a3ea00f50da6693775d8085ca8e2695e21ada2f
mstepinski <mikistep20@gmail.com>
2025-08-29 13:12:56 +0100

    Iniatial codeforeces commit
    The debug macros have been copied from tourist's codeforces submissions.
    For example https://codeforces.com/contest/1208/submission/59484058

---
+++ codeforces/template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#pragma GCC OPTIMIZE("O3")
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
+void solve() {
+  cout <<"asdf\n";
+  debug(123);
+}
+
+int main() {
+  int q;
+  cin >>q;
+  while(q--) {
+    solve();
+  }
+}

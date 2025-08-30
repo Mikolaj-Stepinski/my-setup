9347e6e354f6d44c995ba94bc398b77fc30d223b
mstepinski <mikistep20@gmail.com>
2025-08-30 20:02:09 +0100

    more codeforces things

--- codeforces/template.cpp
+++ codeforces/template.cpp
@@ -2,6 +2,11 @@
 #pragma GCC OPTIMIZE("O3")
 using namespace std;
 
+#define FOR(i,n) for(int i=0;i<n;i++)
+typedef int64_t ll;
+typedef uint64_t ull;
+constexpr ll MOD = 998244353;
+
 #ifdef DEBUG
 #include "debug.h"
 #else
@@ -9,8 +14,7 @@ using namespace std;
 #endif
 
 void solve() {
-  cout <<"asdf\n";
-  debug(123);
+  
 }
 
 int main() {

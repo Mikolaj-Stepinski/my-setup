1293fa52244b59b44e240fb31f4706c152fc4413
Mikołaj Stępiński <mikistep2@gmail.com>
2026-01-06 22:31:06 +0000

    windows setup

---
+++ windows/profile.ps1
@@ -0,0 +1,2 @@
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
---
+++ windows/setup.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy Unrestricted
+winget install fzf
+Install-Module -Name PSFzf
